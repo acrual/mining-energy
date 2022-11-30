b1bb2adcbb54c27813b2fbc5fb9b1b8c5af6d8fa
Adolfo Contreras <contreras.adolfo@gmail.com>
2022-11-30 20:39:11 +0100

    añadiendo charts preliminares con matplotlib

--- frikada3.py
+++ frikada3.py
@@ -26,49 +26,23 @@ def addRows(ppio, fin, tasa, df2):
 
 df2 = addRows(idActual, idBMNfinal, tasaPositiva, df2)
 print(df2[-30:])
-# def chartIt(opcion):
-#     if opcion == 1:
-#         x1 = df2['date'][idInicial:idFinal]
-#         actuals, forecast = x1 <= now, x1 >= now
-#         visible, invisible = x1 <= now, x1 <= "13/01/2022"
-#         y1 = buy[0][0][idInicial:idFinal]
-#         y2 = mine[2][idInicial:idFinal]
-#         cols = ['BMN price', 'bitcoin_price']
-#         x2 = df2['date'][idBMNInicial:idBMNFinal]
-#         df2['BMNUSD'] = df2.loc[:, cols].prod(axis=1)
-#         # print("probando esto: ", df2['BMNUSD'][idBMNInicial - 5:idBMNInicial+50])
-#         # df2['BMNUSD']= df2['BMNUSD'].replace(0.0000,'')
-#         y3 = df2['BMNUSD'][idInicial:idFinal]
-#         plt.plot(x1[actuals], y1[actuals], '-', label='buy&hodl-Actuals')
-#         plt.plot(x1[forecast], y1[forecast], '--', label='buy&hodl-Forecast')
-#         plt.plot(x1[actuals], y2[actuals], '-', label='mine&hodl-Actuals')
-#         plt.plot(x1[forecast], y2[forecast], '--', label='mine&hodl-Forecast')
-#         plt.plot(x1[visible], y3[visible], '-', label='BMNUSD Price')
-#         plt.plot(x1[invisible], y3[invisible], '--', label='BMNUSD Price')
-#         plt.xlabel('days running')
-#         plt.ylabel('USD')
-#         plt.title('Mine vs Buy for BMN in USD')
-#         plt.legend()
-#         plt.show()
-#         plt.close('all')
-#
-# def menu():
-#     opcion = 100
-#     while True:
-#         print("1. Mine vs Hodl in USD for BMN for tranche 1")
-#         print("2. Mine vs Hodl in BTC for BMN")
-#         print("3. Network Hashrate (line) and accumulated BTC (bars)")
-#         print("4. Mine vs Hodl in USD for BMN for all tranches")
-#         print("5. 1 pero no BMN")
-#         print("6. 2 pero no BMN")
-#         print("7. 3 pero no BMN")
-#         print("8. 4 pero no BMN")
-#         print("9. Export Dataframe to CSV")
-#         print("0. Salir")
-#         opcion = input("¿Qué gráfica quieres?: ")
-#         if opcion != '0':
-#             buildChart(int(opcion))
-#         else:
-#             break
-#
-# menu()
+def chartIt(idBMNfinal):
+        x1 = df2['date'][idInicial:idBMNfinal]
+        actuals, forecast = x1 <= now, x1 >= now
+        # visible, invisible = x1 <= now, x1 <= "13/01/2022"
+        y1 = df2['buy1'][idInicial:idBMNfinal]
+        y2 = df2['mined'][idInicial:idBMNfinal]
+        x2 = df2['date'][idBMNInicial:idBMNfinal]
+        plt.plot(x1[actuals], y1[actuals], '-', label='buy&hodl-Actuals')
+        plt.plot(x1[forecast], y1[forecast], '--', label='buy&hodl-Forecast')
+        plt.plot(x1[actuals], y2[actuals], '-', label='mine&hodl-Actuals')
+        plt.plot(x1[forecast], y2[forecast], '--', label='mine&hodl-Forecast')
+        # plt.plot(x1[visible], y3[visible], '-', label='BMNUSD Price')
+        # plt.plot(x1[invisible], y3[invisible], '--', label='BMNUSD Price')
+        plt.xlabel('days running')
+        plt.ylabel('USD')
+        plt.title('Mine vs Buy for BMN in USD')
+        plt.legend()
+        plt.show()
+        plt.close('all')
+chartIt(idBMNfinal)
